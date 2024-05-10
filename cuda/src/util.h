73db364001d72fae07c4b8a910bb4ea3c415735c
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2024-05-10 16:46:58 +0900

    Makefile for clang++

--- cuda/src/util.h
+++ cuda/src/util.h
@@ -58,26 +58,3 @@ void printVector(const char* label, const vector &v)
   std::cout  << std::endl;
 }
 
-#if defined(__CUDA_ARCH__) && __CUDA_ARCH__ < 600 
-// Double precision atomicAdd in software
-static 
-__device__ 
-__forceinline__
-double atomicAdd(double* address, double val)
-{
-  unsigned long long int* address_as_ull =
-    (unsigned long long int*)address;
-  unsigned long long int old = *address_as_ull, assumed;
-
-  do {
-    assumed = old;
-    old = 
-      atomicCAS(address_as_ull, assumed,
-        __double_as_longlong(val +
-          __longlong_as_double(assumed)));
-  } while (assumed != old);
-
-  return __longlong_as_double(old);
-
-}
-#endif
